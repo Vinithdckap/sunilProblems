2a1dcecab18a7b7923b37464210ea2f40626b00c
Vinithdckap <vinithkdckap@gmail.com>
2023-05-31 14:34:09 +0530

    swapped two numbers

---
+++ swap.php
@@ -0,0 +1,8 @@
+<?php
+
+$firstNumber = readline("enter first number =");
+$secondNumber = readline("enter second Number =");
+$firstNumber = $firstNumber + $secondNumber;
+$secondNumber = $firstNumber - $secondNumber;
+
+echo "firstNumber=".$firstNumber." firstNumbernd  =".$secondNumber;
