b1bf62adfa230bf02c6b385c204183a1f2fc0d1f
Vinithdckap <vinithkdckap@gmail.com>
2023-06-02 10:20:09 +0530

    sorted successfully

---
+++ sortInteger.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$array = array(1,2,3,4,5,7,8,3,2);
+
+// sorting the array in descending order;;
+rsort($array);
+print_r($array);
+
+// sorting the array in ascending order;
+sort($array);
+print_r($array);
+
+
